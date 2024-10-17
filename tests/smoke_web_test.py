f1356454aa705ed6eb0e6ef0a3b2e040b9ab2333
Bartek Wilczek <bwilczek@gmail.com>
2024-10-17 15:30:24 +0200

    Reusable components

--- tests/smoke_web_test.py
+++ tests/smoke_web_test.py
@@ -14,6 +14,21 @@ class GreetingPage:
         self.name_input.fill(name)
         self.set_name_button.click()
 
+class ToDoListComponent:
+    def __init__(self, root):
+        self.root = root
+        self.title = self.root.locator("xpath=//*[@role='title']")
+
+class MultiToDoListPage:
+    def __init__(self, page):
+        self.page = page
+
+    def navigate(self):
+        self.page.goto("https://bwilczek.github.io/watir_pump_tutorial/todo_lists.html")
+
+    def lists(self):
+        return [ ToDoListComponent(list) for list in self.page.locator("xpath=//*[@role='todo_list']").all() ]
+
 def test_has_title(page: Page):
     app = GreetingPage(page)
     app.navigate()
@@ -21,3 +36,8 @@ def test_has_title(page: Page):
 
     expect(app.greeting_text).to_contain_text("Hello Marzena!")
 
+def test_multi_todo_lists(page: Page):
+    app = MultiToDoListPage(page)
+    app.navigate()
+
+    expect(app.lists()[0].title).to_contain_text ("Home")
