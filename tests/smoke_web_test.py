37640b8f2126d05942b83b0f9b07a088966ab720
Bartek Wilczek <bwilczek@gmail.com>
2024-10-17 14:03:32 +0200

    Use Page Object

--- tests/smoke_web_test.py
+++ tests/smoke_web_test.py
@@ -1,9 +1,23 @@
-import re
 from playwright.sync_api import Page, expect
 
-def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
+class GreetingPage:
+    def __init__(self, page):
+        self.page = page
+        self.name_input = page.locator('#name')
+        self.set_name_button = page.locator('#set_name')
+        self.greeting_text = page.locator('#greeting')
 
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
+    def navigate(self):
+        self.page.goto("https://bwilczek.github.io/watir_pump_tutorial/greeter.html")
+
+    def greet(self, name):
+        self.name_input.fill(name)
+        self.set_name_button.click()
+
+def test_has_title(page: Page):
+    app = GreetingPage(page)
+    app.navigate()
+    app.greet("Marzena")
+
+    expect(app.greeting_text).to_contain_text("Hello Marzena!")
 
