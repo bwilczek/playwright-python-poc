51bdb4167e8c2601cd9f0e4a2db5dc22b8d6fe57
Bartek Wilczek <bartek.wilczek@toptal.com>
2024-10-18 09:53:28 +0200

    Expand POM

--- tests/smoke_web_test.py
+++ tests/smoke_web_test.py
@@ -14,10 +14,36 @@ class GreetingPage:
         self.name_input.fill(name)
         self.set_name_button.click()
 
+
+class ToDoListItemComponent:
+    def __init__(self, root: Locator):
+        self.root = root
+        self.name = self.root.locator("xpath=./*[@role='name']")
+        self.remove_button = self.root.locator("xpath=./*[@role='rm']")
+
+    def remove(self):
+        self.remove_button.click()
+
+
 class ToDoListComponent:
     def __init__(self, root: Locator):
         self.root = root
         self.title = self.root.locator("xpath=./*[@role='title']")
+        self.add_button = self.root.locator("xpath=./*[@role='add']")
+        self.new_item_input = self.root.locator("xpath=./*[@role='new_item']")
+
+    @property
+    def items(self):
+        items_list = [ ToDoListItemComponent(list) for list in self.root.get_by_role("listitem").all() ]
+        return { item.name.text_content() : item for item in items_list }
+
+    @property
+    def items_as_text(self):
+        return self.items.keys()
+
+    def add_item(self, item: str):
+        self.new_item_input.fill(item)
+        self.add_button.click()
 
 class MultiToDoListPage:
     def __init__(self, page: Page):
@@ -43,3 +69,10 @@ def test_multi_todo_lists(page: Page):
     app.navigate()
 
     expect(app.lists["Home"].title).to_contain_text ("Home")
+    expect(app.lists["Home"].items["Dishes"].name).to_be_visible()
+
+    app.lists["Home"].add_item("Shopping")
+    assert "Shopping" in app.lists["Home"].items_as_text
+
+    app.lists["Home"].items["Vacuum"].remove()
+    assert "Vacuum" not in app.lists["Home"].items_as_text
